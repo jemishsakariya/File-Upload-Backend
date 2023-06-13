11ec59efba29b6a2954b88410626c965cdfe45cc
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-13 16:02:10 +0530

    imageReducerUpload is done

--- Controller/fileUpload.js
+++ Controller/fileUpload.js
@@ -38,9 +38,12 @@ function isFileTypeSupported(type, supportedTypes) {
   return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = { folder };
   options.resource_type = "auto";
+  if (quality) {
+    options.quality = quality;
+  }
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -130,4 +133,41 @@ exports.videoUpload = async (req, res) => {
 };
 
 // it upload files on cloudinary server
-exports.imageReducerUpload = async (req, res) => {};
+exports.imageReducerUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+
+    const file = req.files.imageFile;
+
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File type is not supported",
+      });
+    }
+
+    const response = await uploadFileToCloudinary(file, "fileUpload", 10);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Image Successfully Uploaded",
+      imageUrl: response.secure_url,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
