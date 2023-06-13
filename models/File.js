3c689d0bf035f42b754d1cc0b1de1edfc9cc9cdc
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-13 20:33:27 +0530

    Send mail using nodemailer & imported with external file & use Template for mail

--- models/File.js
+++ models/File.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+// const nodemailer = require("nodemailer");
+const transporter = require("../config/transporter");
+
+const mailTemplate = require("../Template/mailTemplate");
+
 const fileSchema = mongoose.Schema({
   name: {
     type: String,
@@ -14,6 +19,30 @@ const fileSchema = mongoose.Schema({
   tags: {
     type: String,
   },
+});
+
+// post middleware
+// doc => when you send req and you store something in your Mongodb it is the doc argument
+fileSchema.post("save", async function (doc) {
+  try {
+    // console.log("Doc: ", doc);
+
+    // transporter
+    // transporter => imported with external file
+
+    // mail send
+    let info = await transporter.sendMail({
+      from: "Testing",
+      to: doc.email,
+      subject: "New File Uploaded on Cloudinary",
+      html: mailTemplate(doc),
+      // html: `<h2>Hello</h2></br><p>File Uploaded</p>`,
+    });
+
+    console.log("Info: ", info);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const File = mongoose.model("File", fileSchema);
