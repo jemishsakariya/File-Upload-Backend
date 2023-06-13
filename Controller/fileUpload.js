8c4e9312019b755a945ceae9305b90d779296974
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-13 11:39:32 +0530

    Revert "localFileUpload Done"
    This reverts commit 2a5bb3a5dd2d4dde3acdd689b0fd26ded9683fcf.

--- Controller/fileUpload.js
+++ Controller/fileUpload.js
@@ -1,42 +1,7 @@
-const File = require("../models/File");
-
-// it upload files on your server
-exports.localFileUpload = async (req, res) => {
-  try {
-    // fetch the file
-    // .file = is passed in postman api in Body>form-data> file -> Value
-    const file = req.files.file;
-    // console.log("file: ", file);
-
-    // where you wanna store
-    // "__dirname" => "current directory (Controller)"
-    // path = current_dir + folder + name + extention
-    const path =
-      __dirname + "/files/" + Date.now() + `.${file.name.split(".")[1]}`;
-    // console.log("path: ", path);
-
-    file.mv(path, (err) => {
-      console.log(err);
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Local File Uploaded Successfully",
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-    });
-  }
-};
-
-// it upload files on cloudinary server
 exports.imageUpload = async (req, res) => {};
 
-// it upload files on cloudinary server
 exports.videoUpload = async (req, res) => {};
 
-// it upload files on cloudinary server
 exports.imageReducerUpload = async (req, res) => {};
+
+exports.localFileUpload = async (req, res) => {};
